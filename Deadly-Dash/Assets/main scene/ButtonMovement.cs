93914f19704b29f1c8d9879e5a150c5d397b6aaf
Edward <s182377@students.aie.edu.au>
2018-11-19 13:53:24 +1100

    smooth movement
    smooth movement

--- Deadly-Dash/Assets/main scene/ButtonMovement.cs
+++ Deadly-Dash/Assets/main scene/ButtonMovement.cs
@@ -17,52 +17,83 @@ public class ButtonMovement : MonoBehaviour
     public GameObject Middle;
     public GameObject Right;
 
-    [Range(1,20)]
-    public float speed = 8.0f;
+    [Range(0.05f,1.5f)]
+    public float MoveTime = 0.5f;
+
+    [Range(1,30)]
+    public float JumpPower = 2;
 
 
 
     private GameObject currentNode;
+    private GameObject previousNode;
+
     private TargetPosition currentPosition = TargetPosition.Middle;
+
+    public AnimationCurve MoveCurve;
+    
+
+    private float currentMoveTimer = 0;
+
 	// Use this for initialization
 	void Start ()
     {
         currentNode = Middle;
+        previousNode = currentNode;
         transform.position = Middle.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Vector3.MoveTowards(transform.position, currentNode.transform.position, speed * Time.deltaTime);
+        currentMoveTimer += Time.deltaTime;
+        currentMoveTimer = Mathf.Clamp(currentMoveTimer, 0, MoveTime);
+
+        float normalizedTime = currentMoveTimer / MoveTime;
+
+        float normalizedMoveDistance = MoveCurve.Evaluate(normalizedTime);
+        transform.position = Vector3.Lerp(previousNode.transform.position, currentNode.transform.position, normalizedMoveDistance);
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, currentNode.transform.position, speed * Time.deltaTime);
+        //float remainingTravelDistance = Vector3.Distance(transform.position, currentNode.transform.position);
+
+
+        //float travelPercent = remainingTravelDistance / travelNodeDistance;
     }
 
     public void MoveLeft()
     {
+        if (currentMoveTimer < MoveTime - 0.001f) return;
         if(currentPosition == TargetPosition.Middle)
         {
-            SetTarget(TargetPosition.Left);
-           
+            SetTarget(TargetPosition.Left); 
         }
-        if(currentPosition == TargetPosition.Right)
+        else if (currentPosition == TargetPosition.Right)
         {
             SetTarget(TargetPosition.Middle);
-            
-        }
-        
+        }   
     }
     public void MoveRight()
     {
-      if(currentPosition == TargetPosition.Middle)
+        if (currentMoveTimer < MoveTime - 0.001f) return;
+        if (currentPosition == TargetPosition.Middle)
         {
             
             SetTarget(TargetPosition.Right);
            
         }
-      if(currentPosition == TargetPosition.Left)
+      else if(currentPosition == TargetPosition.Left)
         {
             SetTarget(TargetPosition.Middle);
-            currentNode = Middle;
         }
         
     }
@@ -70,17 +101,20 @@ public class ButtonMovement : MonoBehaviour
 
     private void SetTarget(TargetPosition targetPosition)
     {
-        if(targetPosition == TargetPosition.Left)
+        previousNode = currentNode;
+        currentMoveTimer = 0.0f;
+        if (targetPosition == TargetPosition.Left)
         {
+            
             currentNode = Left;
             currentPosition = TargetPosition.Left;
         }
-        if(targetPosition == TargetPosition.Middle)
+        else if(targetPosition == TargetPosition.Middle)
         {
             currentNode = Middle;
             currentPosition = TargetPosition.Middle;
         }
-        if(targetPosition == TargetPosition.Right)
+        else if(targetPosition == TargetPosition.Right)
         {
             currentNode = Right;
             currentPosition = TargetPosition.Right;
@@ -88,7 +122,13 @@ public class ButtonMovement : MonoBehaviour
         //transform.position = currentNode.transform.position;
     }
 
-
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+        }
+    }
 
 
 }
