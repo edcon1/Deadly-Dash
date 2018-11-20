11bd529e779ffaef864fd9c7dfbba5952321a0c2
Edward <s182377@students.aie.edu.au>
2018-11-20 11:43:17 +1100

    working canvas, movement and generation with all assests having material

---
+++ Deadly-Dash/Assets/Scripts/Damage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    DeathMenu deathMenu;
+
+    // Use this for initialization
+    void Start ()
+    {
+        deathMenu = GameObject.Find("DeathMenu").GetComponent<DeathMenu>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Damage")
+        {
+            deathMenu.Death();
+        }
+    }
+}
---
+++ Deadly-Dash/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool IsGamePause = false;
+
+    public GameObject pauseMenuUI;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsGamePause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePause = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePause = true;
+    }
+
+    public void LoadHighScore()
+    {
+        SceneManager.LoadScene("HighScore");
+    }
+    
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
+    }
+}
